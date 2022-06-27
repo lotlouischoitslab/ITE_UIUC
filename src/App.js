e331276d3cfdb3746e1a167a4abfb33e1ae3ae0e
lotlouischoitslab <louis.sw.cho@gmail.com>
2022-06-27 00:01:26 -0500

    stop for now

--- src/App.js
+++ src/App.js
@@ -4,16 +4,14 @@ import Home from './components/Home';
 import About from './components/About';
 import Team from './components/Team';
 import Contact from './components/Contact';
+import {Route,Link} from 'react-router-dom';
 
 
 function App() {
   return (
     <div>
       <Navbar/>
-      <Home/>
-      <About/>
-      <Team/>
-      <Contact/>
+      {/* <Route to= '/home' components={Home} /> */}
       
     </div>
   );
