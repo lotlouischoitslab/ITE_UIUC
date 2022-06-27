ba505f437a1ae7d409d8f96d9a63a74e6d18c612
lotlouischoitslab <louis.sw.cho@gmail.com>
2022-06-26 23:13:05 -0500

    commit all

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='fixed w-full h-[80px] flex justify-between items-centered px-4 bg-[blue]'>
+
+    </div>
+  )
+}
+
+export default Navbar;
