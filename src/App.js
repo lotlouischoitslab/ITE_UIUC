19d412e62e55cec2bd81bd78db79daaa288ce964
lotlouischoitslab <louis.sw.cho@gmail.com>
2022-06-26 23:49:13 -0500

    add all

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import React from 'react';
 import Navbar from './components/Navbar'
+import Home from './components/Home';
+import About from './components/About';
+import Team from './components/Team';
+import Contact from './components/Contact';
+
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <Home/>
+      <About/>
+      <Team/>
+      <Contact/>
       
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import React from 'react';
+import {FaBars,FaTimes} from 'react-icons/fa';
+import ITE_LOGO from '../assets/ite_logo.png';
 
 const Navbar = () => {
   return (
-    <div className='fixed w-full h-[80px] flex justify-between items-centered px-4 bg-[blue]'>
+    <div className='fixed w-full h-[60px] flex justify-between items-centered px-4 py-2 bg-[#29b6f6] text-[white]'>
+        <div>
+            <img src={ITE_LOGO} alt="Logo Image" style={{width:'50px'}} />
+        </div>
+
+        {/* MENU */}
+
+        <div>
+            <ul className='flex'>
+                <li>HOME</li>
+                <li>ABOUT</li>
+                <li>TEAM</li>
+                <li>CONTACT</li>
+                <li>JOIN</li>
+            </ul>
+        </div>
+
+        {/* HAMBURGER */}
+        <div className="hidden">
+            <FaBars/>
+        </div>
+        
+        {/* MOBILE MENU*/}
+        <ul className="hidden">
+            <li>HOME</li>
+            <li>ABOUT</li>
+            <li>TEAM</li>
+            <li>CONTACT</li>
+            <li>JOIN</li>
+        </ul>
+
+
 
     </div>
   )
